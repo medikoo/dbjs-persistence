1d2c67a7daa582fcd3177929289b7551d6ca7534
Mariusz Nowak <medyk@medikoo.com>
2015-10-22 12:57:54 +0200

    Ensure promise gets returned from close()

--- abstract.js
+++ abstract.js
@@ -174,7 +174,7 @@ ee(Object.defineProperties(PersistenceDriver.prototype, assign({
 			this._closeDeferred = deferred();
 			return this._closeDeferred.promise;
 		}
-		return this._close();
+		return deferred(this._close());
 	}),
 	_ensureOpen: d(function () {
 		if (this.isClosed) throw new Error("Database not accessible");
