a665290596faf6a81f5b5a16da13494a603df390
Mariusz Nowak <medyk@medikoo.com>
2015-10-25 11:15:08 +0100

    Do not support functions in search value filter

--- test/lib/get-search-value-filter.js
+++ test/lib/get-search-value-filter.js
@@ -4,7 +4,6 @@ module.exports = function (t, a) {
 	a(t()(''), false);
 	a(t()('0'), false);
 	a(t()('11'), true);
-	a(t(Boolean), Boolean);
 	a(t('3raz')('22'), false);
 	a(t('3raz')('3foo'), false);
 	a(t('3raz')('3raz'), true);
