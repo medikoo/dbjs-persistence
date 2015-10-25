0a9d0485367dd3ac726810a11400e50ae8aa6709
Mariusz Nowak <medyk@medikoo.com>
2015-10-25 10:32:03 +0100

    Introduce onFlush promise

--- abstract.js
+++ abstract.js
@@ -209,11 +209,20 @@ ee(Object.defineProperties(PersistenceDriver.prototype, assign({
 		if (this.isClosed) throw new Error("Database not accessible");
 	}),
 	_runningOperations: d(0),
+	onFlush: d.gs(function () {
+		if (!this._runningOperations) return deferred(undefined);
+		if (!this._onFlush) this._onFlush = deferred();
+		return this._onFlush.promise;
+	}),
 	_close: d(notImplemented)
 }, autoBind({
 	emitError: d(emitError),
 	_onOperationEnd: d(function () {
 		if (--this._runningOperations) return;
+		if (this._onFlush) {
+			this._onFlush.resolve();
+			delete this._onFlush;
+		}
 		if (!this._closeDeferred) return;
 		this._closeDeferred.resolve(this._close());
 	})
