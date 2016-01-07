709b6da02577664d16adaccd55ca13f6ff5ff779
Mariusz Nowak <medyk@medikoo.com>
2016-01-07 12:52:02 +0100

    Rename is to isStorage

--- ensure.js
+++ ensure.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var is = require('./is');
+var is = require('./is-storage');
 
 module.exports = function (x) {
 	if (!is(x)) throw new TypeError(x + " is not a dbjs persistency driver");
