0028625dadc2bfeada8f63f04ac7b57e06d3c9f5
Mariusz Nowak <medyk@medikoo.com>
2016-02-02 22:28:38 +0100

    Pass option for driver in handler.getDriver

--- emitter/index.js
+++ emitter/index.js
@@ -2,19 +2,20 @@
 
 'use strict';
 
-var toArray        = require('es5-ext/array/to-array')
-  , ensureIterable = require('es5-ext/iterable/validate-object')
-  , assign         = require('es5-ext/object/assign')
-  , ensureString   = require('es5-ext/object/validate-stringifiable-value')
-  , Map            = require('es6-map')
-  , d              = require('d')
-  , lazy           = require('d/lazy')
-  , deferred       = require('deferred')
-  , once           = require('timers-ext/once')
-  , ensureDatabase = require('dbjs/valid-dbjs')
-  , emitter        = require('../lib/emitter')
-  , receiver       = require('../lib/receiver')
-  , Driver         = require('./driver')
+var toArray          = require('es5-ext/array/to-array')
+  , ensureIterable   = require('es5-ext/iterable/validate-object')
+  , assign           = require('es5-ext/object/assign')
+  , normalizeOptions = require('es5-ext/object/normalize-options')
+  , ensureString     = require('es5-ext/object/validate-stringifiable-value')
+  , Map              = require('es6-map')
+  , d                = require('d')
+  , lazy             = require('d/lazy')
+  , deferred         = require('deferred')
+  , once             = require('timers-ext/once')
+  , ensureDatabase   = require('dbjs/valid-dbjs')
+  , emitter          = require('../lib/emitter')
+  , receiver         = require('../lib/receiver')
+  , Driver           = require('./driver')
 
   , isIdent = RegExp.prototype.test.bind(/^[a-z][a-z0-9A-Z]*$/)
   , create = Object.create, stringify = JSON.stringify;
@@ -38,13 +39,12 @@ var EmitterHandler = module.exports = function (database) {
 };
 
 Object.defineProperties(EmitterHandler.prototype, assign({
-	getDriver: d(function (name) {
+	getDriver: d(function (name/*, options*/) {
 		var driver;
 		name = ensureString(name);
 		if (!isIdent(name)) throw new TypeError(stringify(name) + " is an invalid storage name");
 		if (this._drivers[name]) return this._drivers[name];
-		driver = this._drivers[name] = new Driver(this);
-		driver.name = name;
+		driver = this._drivers[name] = new Driver(this, normalizeOptions(arguments[1], { name: name }));
 		driver._loadedEventsMap = this._loadedEventsMap;
 		return driver;
 	}),
