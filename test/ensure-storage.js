54f49d011df9842ecb28bd16c01075c32a56d735
Mariusz Nowak <medyk@medikoo.com>
2016-02-02 11:25:06 +0100

    Fix var name

--- test/ensure-storage.js
+++ test/ensure-storage.js
@@ -3,12 +3,12 @@
 var Driver = require('../driver');
 
 module.exports = function (t, a) {
-	var db = new Driver(), storage = db.getStorage('base');
+	var driver = new Driver(), storage = driver.getStorage('base');
 	a.throws(function () { t(); }, TypeError);
 	a.throws(function () { t(null); }, TypeError);
 	a.throws(function () { t(true); }, TypeError);
 	a.throws(function () { t('sdfss'); }, TypeError);
 	a.throws(function () { t({}); }, TypeError);
-	a.throws(function () { t(db); }, TypeError);
+	a.throws(function () { t(driver); }, TypeError);
 	a(t(storage), storage);
 };
