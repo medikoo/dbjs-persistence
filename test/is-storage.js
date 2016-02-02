225dfe85588767076af8975ad9994889ea04f987
Mariusz Nowak <medyk@medikoo.com>
2016-02-02 11:24:34 +0100

    Fix var name

--- test/is-storage.js
+++ test/is-storage.js
@@ -3,12 +3,13 @@
 var Driver = require('../driver');
 
 module.exports = function (t, a) {
-	var db = new Driver(), storage = db.getStorage('base');
+	var driver = new Driver(), storage = driver.getStorage('base');
 	a(t(), false);
 	a(t(null), false);
 	a(t(true), false);
 	a(t('sdfss'), false);
 	a(t('sdfss'), false);
-	a(t(db), false);
+	a(t(driver), false);
+	a(t(storage), true);
 	a(t(storage), true);
 };
