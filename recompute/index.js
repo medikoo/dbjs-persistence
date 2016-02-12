8413627fb1ac91dec48bdb2faedaf4380d071141
Mariusz Nowak <medyk@medikoo.com>
2016-02-12 14:40:13 +0100

    Fix direct errors handling

--- recompute/index.js
+++ recompute/index.js
@@ -112,7 +112,11 @@ module.exports = function (driver, data) {
 						return flatten.call(data).sort(byStamp);
 					})(emitData)(function (data) {
 						data.events.forEach(function (data) {
-							indexesData[data.name][data.ns][data.path] = data;
+							if (data.type === 'direct') {
+								directData[data.ns + (data.path ? ('/' + data.path) : '')] = data;
+							} else {
+								indexesData[data.name][data.ns][data.path] = data;
+							}
 						});
 						return sendData(data.health);
 					});
