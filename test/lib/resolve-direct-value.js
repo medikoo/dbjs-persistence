1be6f3f5743ed5d1c76692a4c51f613c0eca81e1
Mariusz Nowak <medyk@medikoo.com>
2017-05-18 13:36:30 +0200

    fix: direct value resolution

--- test/lib/resolve-direct-value.js
+++ test/lib/resolve-direct-value.js
@@ -1,9 +1,13 @@
 'use strict';
 
+var serializeKey = require('dbjs/_setup/serialize/key');
+
 module.exports = function (t, a) {
 	a(t('foo', null, '3elo'), '3elo');
 	a(t('foo', 'marko', '3elo'), '3elo');
 	a(t('foo', 'marko', '11'), '11');
 	a(t('foo', 'marko*bar', '11'), '3bar');
 	a(t('foo', 'marko*21', '11'), '21');
+	a(t('foo', 'marko*' + serializeKey('bar'), '11'), '3bar');
+	a(t('foo', 'marko*' + serializeKey('bar foo\n'), '11'), '3bar foo\\n');
 };
